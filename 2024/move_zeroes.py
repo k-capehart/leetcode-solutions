f8ff7faccd0c962ced9361e65c2f14fce571b238
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-02-23 09:41:43 -0500

    improved move_zeroes

--- 2024/move_zeroes.py
+++ 2024/move_zeroes.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        x = 0
-        y = len(nums)
-        while x < y:
-            if nums[x] == 0:
-                nums.pop(x)
-                nums.append(0)
-                y = y - 1
-            else:
-                x = x + 1
+        nums_length = len(nums)
+        if nums_length <= 1:
+            return
+
+        bound = 0
+        for i in range(1, nums_length):
+            if nums[bound] != 0:
+                bound += 1
+            if nums[i] != 0:
+                nums[bound], nums[i] = nums[i], nums[bound]
