b2bc825411cb729544a3e4b697ce231494bf490a
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-02-29 13:56:19 -0500

    dota 2 senate - improved

--- 2024/dota2_senate.py
+++ 2024/dota2_senate.py
@@ -1,22 +1,22 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        r_list = []
-        d_list = []
+        r_count = 0
+        d_count = 0
 
         while "D" in senate and "R" in senate:
             new_senate = ""
             for senator in senate:
                 if (senator == 'D'):
-                    if (r_list):
-                        r_list.pop()
+                    if (r_count):
+                        r_count -= 1
                     else:
-                        d_list.append(1)
+                        d_count += 1
                         new_senate += 'D'
                 if (senator == 'R'):
-                    if (d_list):
-                        d_list.pop()
+                    if (d_count):
+                        d_count -= 1
                     else:
-                        r_list.append(1)
+                        r_count += 1
                         new_senate += 'R'
             senate = new_senate
 
