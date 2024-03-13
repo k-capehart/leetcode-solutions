134e524a71d2cd392124a263cc60c1381a0d4c4f
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-03-13 10:19:03 -0400

    go: leaf similar trees

---
+++ go/leaf-similar-trees.go
@@ -0,0 +1,30 @@
+package main
+
+import "reflect"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	return reflect.DeepEqual(buildLeafValSeq(root2), buildLeafValSeq(root1))
+}
+
+func buildLeafValSeq(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return []int{root.Val}
+	}
+
+	left := buildLeafValSeq(root.Left)
+	right := buildLeafValSeq(root.Right)
+
+	return append(left, right...)
+}
