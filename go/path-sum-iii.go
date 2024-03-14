841406d2e00e75ddabb788cae764f045ce37ea89
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-03-14 11:46:04 -0400

    go: path sum iii

---
+++ go/path-sum-iii.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+
+	return traversePathSum(root, targetSum, 0) + pathSum(root.Left, targetSum) + pathSum(root.Right, targetSum)
+}
+
+func traversePathSum(root *TreeNode, targetSum int, sum int) int {
+	if root == nil {
+		return 0
+	}
+
+	count := 0
+	if root.Val+sum == targetSum {
+		count++
+	}
+
+	lCount := traversePathSum(root.Left, targetSum, sum+root.Val)
+	rCount := traversePathSum(root.Right, targetSum, sum+root.Val)
+
+	return lCount + rCount + count
+}
