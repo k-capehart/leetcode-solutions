8b239a6aaa9d903433aaf907dc092552789e02a7
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-03-15 16:52:10 -0400

    go: cycle length queries in a tree

---
+++ go/cycle-length-queries-in-a-tree.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+func cycleLengthQueries(n int, queries [][]int) []int {
+	var answers []int
+	for _, query := range queries {
+		a := query[0]
+		b := query[1]
+		x1 := int(math.Log2(float64(a)))
+		x2 := int(math.Log2(float64(b)))
+
+		count := int(math.Abs(float64(x2 - x1)))
+		if a > b {
+			a = int(a / int(math.Pow(2, float64(count))))
+		} else {
+			b = int(b / int(math.Pow(2, float64(count))))
+		}
+
+		for a != b {
+			a = int(a / 2)
+			b = int(b / 2)
+			count += 2
+		}
+		answers = append(answers, count+1)
+	}
+	return answers
+}
